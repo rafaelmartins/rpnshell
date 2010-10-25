6c6232baab88d35a4e3636df078a0f155fc06297
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-10-24 23:54:31 -0200

    fixed a bug when the stack is still empty.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 /**
  *  main.c
- *  
+ *
  *  Copyright (c) 2010, Rafael G. Martins <rafael@rafaelmartins.eng.br>
  *  All rights reserved.
- *  
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- *  
+ *
  *  * Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *  * Redistributions in binary form must reproduce the above copyright
@@ -16,7 +16,7 @@
  *  * Neither the name of the author nor the names of its contributors may
  *    be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -28,7 +28,7 @@
  *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 #include <string.h>
@@ -73,7 +73,7 @@ int main(int argc, char* argv){
 #ifdef HAVE_READLINE_HISTORY
         add_history(value);
 #endif
-        if(value[0] == '\0'){
+        if(value[0] == '\0' && stack != NULL){
             float temp2 = stack_pop(&stack);
             stack_push(&stack, temp2);
             stack_push(&stack, temp2);
