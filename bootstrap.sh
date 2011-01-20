7751b7fd21a3ec1ec4ac1ccb4e17a805ae43de24
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2011-01-20 01:58:42 -0200

    fixed build system (configure.ac and bootstrap.sh)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 export WANT_AUTOCONF_2_5="yes"
-export WANT_AUTOMAKE_1_10="yes"
+export WANT_AUTOMAKE_1_11="yes"
 export WANT_AUTOCONF="2.5"
-export WANT_AUTOMAKE="1.10"
+export WANT_AUTOMAKE="1.11"
 
-touch ChangeLog
-libtoolize --force
-
-aclocal -I m4
-autoconf --force
-autoheader --force
-automake --add-missing --force-missing
+autoreconf -W all -i
